8d43acf7ac213f9d7ef7b00de13aa144ab5fef19
schaeferd <schaeferd@c94b853d-e81a-4a99-bfbf-77374e1f4532>
2011-07-27 08:33:19 +0000

    added marcReader2
    git-svn-id: https://culturegraph.svn.sourceforge.net/svnroot/culturegraph/trunk/org.culturegraph.metamorph@322 c94b853d-e81a-4a99-bfbf-77374e1f4532

--- src/main/java/org/culturegraph/metamorph/Morph.java
+++ src/main/java/org/culturegraph/metamorph/Morph.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import org.culturegraph.metamorph.core.MetamorphBuilder;
 import org.culturegraph.metamorph.core.MetamorphException;
 import org.culturegraph.metamorph.readers.MabReader;
+import org.culturegraph.metamorph.readers.MarcReader2;
 import org.culturegraph.metamorph.readers.PicaReader;
 import org.culturegraph.metamorph.readers.RawRecordReader;
 import org.culturegraph.metamorph.streamreceiver.ConsoleWriter;
@@ -42,7 +43,11 @@ public final class Morph {
 	        }else if("mab2".equalsIgnoreCase(extension)){
 	        	reader = new MabReader();
 	        	definition = "src/main/resources/mab2.xml";
-	        }else{
+	        }else if("marc21".equalsIgnoreCase(extension)){
+	        	reader = new MarcReader2();
+	        	definition = "src/main/resources/marc21.xml";
+	        }
+	        else{
 	        	throw new MetamorphException("Extention not recognized");
 	        }
 	
