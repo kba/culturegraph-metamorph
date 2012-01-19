f37ecb4bb07fd61f00dcbe2d39728cdfd10dfe78
walderdbeere <walderdbeere@c94b853d-e81a-4a99-bfbf-77374e1f4532>
2012-01-19 10:36:41 +0000

    bugfix ignore unexpected newlines in CGEntitzReader read()
    git-svn-id: https://culturegraph.svn.sourceforge.net/svnroot/culturegraph/metamorph/branches/release@886 c94b853d-e81a-4a99-bfbf-77374e1f4532

--- src/main/java/org/culturegraph/metamorph/stream/readers/CGEntityReader.java
+++ src/main/java/org/culturegraph/metamorph/stream/readers/CGEntityReader.java
@@ -4,6 +4,8 @@ import java.util.regex.Pattern;
 
 import org.culturegraph.metamorph.stream.CGEntity;
 import org.culturegraph.metamorph.stream.StreamReceiver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class CGEntityReader extends AbstractReader {
 
@@ -11,6 +13,8 @@ public final class CGEntityReader extends AbstractReader {
 			Pattern.LITERAL);
 	private static final Pattern SUBFIELD_PATTERN = Pattern.compile(String.valueOf(CGEntity.SUB_DELIMITER),
 			Pattern.LITERAL);
+	
+	private static Logger LOG = LoggerFactory.getLogger(CGEntityReader.class);
 
 	@Override
 	public String getId(final String record) {
@@ -25,19 +29,21 @@ public final class CGEntityReader extends AbstractReader {
 	}
 
 	public static void read(final String record, final StreamReceiver receiver) {
-		try {
+		try {			
 			final String[] fields = FIELD_PATTERN.split(record);
 			receiver.startRecord(fields[0]);
 			for (int i = 1; i < fields.length; ++i) {
-
-				if (fields[i].charAt(0) == CGEntity.LITERAL_MARKER) {
+				final char firstChar = fields[i].charAt(0);
+				if (firstChar == CGEntity.LITERAL_MARKER) {
 					final String[] parts = SUBFIELD_PATTERN.split(fields[i], -1);
 					receiver.literal(parts[0].substring(1), parts[1].replace(CGEntity.NEWLINE_ESC, CGEntity.NEWLINE));
-				} else if (fields[i].charAt(0) == CGEntity.ENTITY_START_MARKER) {
+				} else if (firstChar == CGEntity.ENTITY_START_MARKER) {
 					receiver.startEntity(fields[i].substring(1));
-				} else if (fields[i].charAt(0) == CGEntity.ENTITY_END_MARKER) {
+				} else if (firstChar == CGEntity.ENTITY_END_MARKER) {
 					receiver.endEntity();
-				} else {
+				} else if(firstChar == CGEntity.NEWLINE){
+					LOG.debug("unexpected newline");
+				}else{
 					throw new RecordFormatException(record);
 				}
 			}
