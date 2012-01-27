bff2c95851e29a4768061372cdfbb4f40215ddd9
boehmec <boehmec@c94b853d-e81a-4a99-bfbf-77374e1f4532>
2012-01-27 15:26:35 +0000

    * Added test-cases to illustrate a problem when handling duplicate entities
    git-svn-id: https://culturegraph.svn.sourceforge.net/svnroot/culturegraph/metamorph/branches/release@909 c94b853d-e81a-4a99-bfbf-77374e1f4532

---
+++ src/test/java/org/culturegraph/metamorph/MetamorphTest.java
@@ -0,0 +1,12 @@
+package org.culturegraph.metamorph;
+
+import org.culturegraph.metamorph.test.MetamorphTestRunner;
+import org.culturegraph.metamorph.test.MetamorphTestRunner.TestDefinitions;
+import org.junit.runner.RunWith;
+
+@RunWith(MetamorphTestRunner.class)
+@TestDefinitions("metamorph-test.xml")
+public final class MetamorphTest {
+	// This class serves only as a bonding
+	// point for metamorph tests	
+}
