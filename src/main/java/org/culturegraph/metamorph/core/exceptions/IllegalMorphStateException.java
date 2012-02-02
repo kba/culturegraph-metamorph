e64c0ba7ff6cd68fb1b77eb5d8eb55050260fa3f
markusgeipel <markusgeipel@c94b853d-e81a-4a99-bfbf-77374e1f4532>
2012-02-02 14:39:07 +0000

    git-svn-id: https://culturegraph.svn.sourceforge.net/svnroot/culturegraph/metamorph/branches/metamorph2@936 c94b853d-e81a-4a99-bfbf-77374e1f4532

---
+++ src/main/java/org/culturegraph/metamorph/core/exceptions/IllegalMorphStateException.java
@@ -0,0 +1,20 @@
+package org.culturegraph.metamorph.core.exceptions;
+
+
+public final class IllegalMorphStateException extends MetamorphException {
+
+	private static final long serialVersionUID = 8738071969657414980L;
+
+	public IllegalMorphStateException(final String message) {
+		super(message);
+	}
+
+	public IllegalMorphStateException(final Throwable cause) {
+		super(cause);
+	}
+
+	public IllegalMorphStateException(final String message, final Throwable cause) {
+		super(message, cause);
+	}
+
+}
