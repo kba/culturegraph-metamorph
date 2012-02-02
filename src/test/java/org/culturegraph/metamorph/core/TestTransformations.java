03e9792a840a71955c8cc6314ce8e2280ef77c11
markusgeipel <markusgeipel@c94b853d-e81a-4a99-bfbf-77374e1f4532>
2012-02-02 14:37:27 +0000

    git-svn-id: https://culturegraph.svn.sourceforge.net/svnroot/culturegraph/metamorph/branches/metamorph2@933 c94b853d-e81a-4a99-bfbf-77374e1f4532

---
+++ src/test/java/org/culturegraph/metamorph/core/TestTransformations.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.culturegraph.metamorph.core;
+
+import org.culturegraph.metamorph.test.MetamorphTestRunner;
+import org.culturegraph.metamorph.test.MetamorphTestRunner.TestDefinitions;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Christoph Böhme <c.boehme@dnb.de>, Markus Geipel
+ *
+ */
+
+@RunWith(MetamorphTestRunner.class)
+@TestDefinitions("test/org.culturegraph.metamorph.core2.xml")
+public final class TestTransformations { 
+	// This class serves only as a bonding
+	// point for metamorph tests
+}
---
+++ src/test/java/org/culturegraph/metamorph/core/functions/LookupTest.java
@@ -0,0 +1,51 @@
+package org.culturegraph.metamorph.core.functions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import junit.framework.Assert;
+
+import org.culturegraph.metamorph.multimap.MultiMap;
+import org.culturegraph.metamorph.multimap.SimpleMultiMap;
+import org.junit.Test;
+
+
+
+/**
+ * tests {@link Lookup}
+ * 
+ * @author Markus Michael Geipel
+ */
+
+public final class LookupTest {
+	
+	private static final String MAP_NAME = "Authors"; 
+	private static final String MAP_NAME_WRONG = "Directors"; 
+	private static final String KEY = "Franz"; 
+	private static final String KEY_WRONG = "Josef"; 
+	private static final String VALUE = "Kafka"; 
+	
+	@Test
+	public void testLookup() {
+		final Lookup lookup = new Lookup();
+		final SimpleMultiMap multiMapProvider = new MultiMap();
+		final Map<String, String> map = new HashMap<String, String>();
+		map.put(KEY, VALUE);
+		
+		multiMapProvider.putMap(MAP_NAME, map);
+		
+		lookup.setMultiMap(multiMapProvider);
+		lookup.setIn(MAP_NAME_WRONG);
+		Assert.assertNull(lookup.process(KEY));
+		Assert.assertNull(lookup.process(KEY_WRONG));
+				
+		lookup.setIn(MAP_NAME);
+		Assert.assertEquals(VALUE, lookup.process(KEY));
+		Assert.assertNull(lookup.process(KEY_WRONG));
+		
+		map.put(SimpleMultiMap.DEFAULT_MAP_KEY, VALUE);
+		Assert.assertEquals(VALUE, lookup.process(KEY_WRONG));
+	}
+	
+	
+}
