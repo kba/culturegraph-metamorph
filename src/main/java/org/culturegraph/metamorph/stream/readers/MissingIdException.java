8c70f12db94a2b1b89e98e9a2565a3bc71dd2780
markusgeipel <markusgeipel@c94b853d-e81a-4a99-bfbf-77374e1f4532>
2012-02-03 13:34:30 +0000

    git-svn-id: https://culturegraph.svn.sourceforge.net/svnroot/culturegraph/metamorph/branches/metamorph2@957 c94b853d-e81a-4a99-bfbf-77374e1f4532

--- src/main/java/org/culturegraph/metamorph/stream/readers/MissingIdException.java
+++ src/main/java/org/culturegraph/metamorph/stream/readers/MissingIdException.java
@@ -2,6 +2,14 @@ package org.culturegraph.metamorph.stream.readers;
 
 import org.culturegraph.metamorph.core.exceptions.MetamorphException;
 
+/**
+ * Thrown by {@link Reader}s if a record has no ID
+ * 
+ * @author Markus Michael Geipel
+ * 
+ * 
+ *
+ */
 public final class MissingIdException extends MetamorphException {
 
 	private static final long serialVersionUID = 2048460214057525724L;
