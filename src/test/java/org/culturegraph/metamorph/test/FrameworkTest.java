0d012bd5f80083917f6afca9dc6eee396d994318
markusgeipel <markusgeipel@c94b853d-e81a-4a99-bfbf-77374e1f4532>
2012-02-06 16:31:33 +0000

    git-svn-id: https://culturegraph.svn.sourceforge.net/svnroot/culturegraph/metamorph/branches/metamorph2@973 c94b853d-e81a-4a99-bfbf-77374e1f4532

--- src/test/java/org/culturegraph/metamorph/test/FrameworkTest.java
+++ src/test/java/org/culturegraph/metamorph/test/FrameworkTest.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(TestSuite.class)
 //@TestDefinitions("test/metamorph-test-1.xml")
-public final class TransformationsTest { 
+public final class FrameworkTest { 
 	// This class serves only as a bonding
 	// point for metamorph tests
 }
