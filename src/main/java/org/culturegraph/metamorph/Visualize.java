5c6ac32da5f4ee8274b6b091f1372968ce198103
markusgeipel <markusgeipel@c94b853d-e81a-4a99-bfbf-77374e1f4532>
2012-02-15 13:03:27 +0000

    git-svn-id: https://culturegraph.svn.sourceforge.net/svnroot/culturegraph/metamorph/branches/release@1035 c94b853d-e81a-4a99-bfbf-77374e1f4532

--- src/main/java/org/culturegraph/metamorph/Visualize.java
+++ src/main/java/org/culturegraph/metamorph/Visualize.java
@@ -1,8 +1,11 @@
 package org.culturegraph.metamorph;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 import org.culturegraph.metamorph.core.MetamorphVisualizer;
 
@@ -13,6 +16,8 @@ import org.culturegraph.metamorph.core.MetamorphVisualizer;
  */
 public final class Visualize {
 
+	private static final String ENCODING = "UTF8";
+
 	private Visualize() {/* no instances */
 	}
 
@@ -22,12 +27,17 @@ public final class Visualize {
 	 * @throws IOException
 	 */
 	public static void main(final String[] args) throws IOException {
-		if (args.length != 1) {
-	
-			System.err.println("Usage: Visualize MORPHDEF");
+		final Writer out;
+		if(args.length==1){
+			out = new OutputStreamWriter(System.out, ENCODING);
+		}else if(args.length==2){
+			System.out.println("Writing to " + args[1]);
+			out = new  OutputStreamWriter(new FileOutputStream(new File(args[1])), ENCODING);
+		}else{
+			System.err.println("Usage: Visualize MORPHDEF [DOTFILE]");
 			return;
 		}
-		final MetamorphVisualizer visualizer = new MetamorphVisualizer(new OutputStreamWriter(System.out, "UTF8"));
+		final MetamorphVisualizer visualizer = new MetamorphVisualizer(out);
 		visualizer.walk(new FileReader(args[0]));
 	}
 
