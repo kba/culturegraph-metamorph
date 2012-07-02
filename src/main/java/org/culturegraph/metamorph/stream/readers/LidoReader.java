eb23d7907941fe28134d38da60a50a9f280d6025
boehmec <boehmec@c94b853d-e81a-4a99-bfbf-77374e1f4532>
2012-07-02 11:15:12 +0000

    *re-merged changes from the release branch in order to add XMLStreamReader
    git-svn-id: https://culturegraph.svn.sourceforge.net/svnroot/culturegraph/metamorph/branches/metamorph-stream@1280 c94b853d-e81a-4a99-bfbf-77374e1f4532

---
+++ src/main/java/org/culturegraph/metamorph/stream/readers/LidoReader.java
@@ -0,0 +1,13 @@
+package org.culturegraph.metamorph.stream.readers;
+
+
+/**
+ * reads XML Lido
+ * @author Markus Michael Geipel
+ * 
+ */
+public class LidoReader extends XMLStreamReader {
+	public LidoReader() {
+		super("lido");
+	}
+}
