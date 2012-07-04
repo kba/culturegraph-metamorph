7f088be17bf0e1f06e215035e93b9b1a75699bc0
markusgeipel <markusgeipel@c94b853d-e81a-4a99-bfbf-77374e1f4532>
2012-07-04 17:11:32 +0000

    refacactored package structure
    git-svn-id: https://culturegraph.svn.sourceforge.net/svnroot/culturegraph/metamorph/trunk@1331 c94b853d-e81a-4a99-bfbf-77374e1f4532

---
+++ src/main/java/org/culturegraph/metamorph/util/registry/ReadOnlyRegistry.java
@@ -0,0 +1,17 @@
+package org.culturegraph.metamorph.util.registry;
+
+/**
+ * Interface for classes which provide access to instances of an arbitrary class <code>T</code> based on a {@link String} key.
+ * 
+ * 
+ * @author Markus Michael Geipel
+ *
+ * @param <T> the type of the objects accessed by a {@link String} key
+ */
+public interface ReadOnlyRegistry<T> {
+	/**
+	 * @param key
+	 * @return Object registered under key
+	 */
+	T get(String key);
+}
